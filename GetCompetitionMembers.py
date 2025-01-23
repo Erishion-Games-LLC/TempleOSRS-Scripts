83e545ea09c09a801dc7b8c09ea5ac1362c2f17a
Ben Colwell <52632729+Ben-Colwell@users.noreply.github.com>
2025-01-23 18:25:45 -0500

    Create GetCompetitionMembers.py

---
+++ GetCompetitionMembers.py
@@ -0,0 +1,48 @@
+import requests
+import time
+import json
+
+temple_API = requests.get("https://templeosrs.com/api/compmembers.php?id=5488")
+
+def checkIdIsInt(Id):
+    try:
+        Id = int(Id)
+        return True
+    except ValueError:
+        return False
+
+def getCompetitionMembers(compID):
+    response = requests.get("https://templeosrs.com/api/compmembers.php?id=" + str(compID))
+
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        playerList = [player.strip('"') for player in data]
+        return playerList
+    else:
+        return None
+    
+def writePlayersToFile(members):
+    for member in members:
+        currentMember = member
+        currentMember = currentMember.replace(" ", "_", -1)
+
+        with open("Members.txt", mode="a") as f:
+            f.writelines(currentMember) 
+            f.write("\n")
+
+def main():
+    while True:
+        compID = input("Enter the desired competition id: ")
+
+        #check that the compID is a valid integer, if not ask for input again
+        if not checkIdIsInt(compID):
+            print("Invalid compID. Please enter a valid integer.")
+            continue #return to input the compID again
+
+        members = getCompetitionMembers(compID)
+        writePlayersToFile(members)
+
+        
+
+if __name__ == "__main__":
+    main()
